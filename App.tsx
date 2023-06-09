76b0029a412383e830ff396f1d5cec1baa252482
Rokas <rookas.rudzenskas@gmail.com>
2023-06-09 18:52:38 +0300

    🎨 style(App.tsx): add a new item to the view with a radio button and a title 🚀 chore(package.json): add react-native-paper dependency to use its components in the app

--- App.tsx
+++ App.tsx
@@ -13,14 +13,36 @@ import {
 function App(): JSX.Element {
 
   return (
-    <>
-
-    </>
+    <View style={styles.container}>
+      <View style={styles.item}>
+        <RadioButton
+          value={'Subscribe to Rokas Rudzianskas'}
+          status={'unchecked'}
+          color="royalblue"
+        />
+        <Text style={styles.itemTitle}>Subscribe to Rokas Rudzianskas</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    padding: 10,
+    flex: 1,
+    backgroundColor: '#211D2D',
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: '#454547',
+    paddingVertical: 5,
+  },
+  itemTitle: {
+    flex: 1,
+    marginLeft: 5,
+  },
 });
 
 export default App;

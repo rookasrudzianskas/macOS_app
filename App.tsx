0622d4c033f402109a6453ba4dd689d1163fc06b
Rokas <rookas.rudzenskas@gmail.com>
2023-06-09 19:24:10 +0300

    🚀 feat(App.tsx): add AsyncStorage support to store and retrieve data 📦 chore(package.json): add @react-native-async-storage/async-storage dependency to use AsyncStorage in App.tsx

--- App.tsx
+++ App.tsx
@@ -71,7 +71,28 @@ function App(): JSX.Element {
     )
   };
 
-  return (
+  const storeData = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem('@storage_Key', jsonValue)
+    } catch (e) {
+      // saving error
+    }
+  }
+
+    const getData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('@storage_Key')
+        if(value !== null) {
+          // value previously stored
+        }
+      } catch(e) {
+        // error reading value
+      }
+    }
+
+
+    return (
     <View style={styles.container}>
       <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
         <Text style={styles.title}>Reminders</Text>
